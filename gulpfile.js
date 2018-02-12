51885693032af75df4c3494327df6730b6be1c6d
alnat2 <alnat2@gmail.com>
2018-02-12 22:21:35 +0300

    add html and css minification

--- gulpfile.js
+++ gulpfile.js
@@ -3,14 +3,22 @@ const gulp = require('gulp');
 const babel = require('gulp-babel');
 const rename = require("gulp-rename");
 const minify = require("gulp-babel-minify");
- 
+const htmlmin = require('gulp-html-minifier');
+const csso = require('gulp-csso');
+const liveServer = require('live-server');
+
+serverParams = {
+	port: 4000,
+	watch: 'dev'
+};
+
 gulp.task('toes5', () =>
 	gulp.src('js/timer.js')
         .pipe(rename('js/timeres5.js'))
         .pipe(babel())
 		.pipe(gulp.dest('.'))
 	);
-gulp.task("minify", () =>
+gulp.task("minjs", () =>
 	gulp.src("js/*.js")
 	  .pipe(minify({
 		mangle: {
@@ -18,4 +26,17 @@ gulp.task("minify", () =>
 		}
 	  }))
 	  .pipe(gulp.dest("build"))
-  );
+	);
+	gulp.task('minhtml', () =>
+		gulp.src('./*.html')
+			.pipe(htmlmin({collapseWhitespace: true}))
+			.pipe(gulp.dest('build'))
+	);
+	gulp.task('mincss', () =>
+    gulp.src('css/*.css')
+      .pipe(csso())
+      .pipe(gulp.dest('build'))
+	);
+gulp.task('watch', () => 
+	liveServer.start(serverParams)
+	);
