6d971c1069489c494d0f603b48574a570e4d4b0f
alnat2 <alnat2@gmail.com>
2018-04-19 13:54:50 +0300

    copy es5 version to root

--- js/timer.js
+++ js/timer.js
@@ -1,1 +1,188 @@
-let countdown,secondsLeft,timers=[];const timersPredefined=['15+5+15+9+15','15+15+15','15+10+15','15+10+15+8+15+10+15'],nextTimers=document.getElementById('nextTimers'),displayArea=document.getElementById('displayArea'),timerInput=document.getElementById('timerInput'),timerRunButton=document.getElementById('timerRun'),timerDisplay=document.querySelector('.displayTimeLeft'),timerTotalTime=document.querySelector('.displayTimeTotal'),soundComplete=document.getElementById('complete');function countdownOver(){soundComplete.play()}function secondsToTimeLeftString(a){const b=Math.floor(a/60),c=a%60,d=`${b}:${10>c?'0':''}${c}`;return d}function displayTimeLeft(a){let b=secondsToTimeLeftString(a);document.title=b,timerDisplay.textContent=b}function displayTotalTime(a){if(1==a.length)return void(timerTotalTime.textContent='');let b=a.reduce((b,a)=>parseInt(b)+parseInt(a));timerTotalTime.textContent=`total timers time is: ${b} ${1==b?'minute':'minutes'}`}function displayTimersPredefined(a){const b=document.createDocumentFragment();for(let c=0;c<a.length;c++){const d=document.createElement('button');d.className='display__time-predefined',d.innerHTML=a[c],b.appendChild(d)}displayArea.appendChild(b)}function displayTimersPredefinedSelect(a){const b=document.createDocumentFragment(),c=document.createElement('select');c.setAttribute('id','predefinedSelect');for(let b=0;b<a.length;b++){const d=document.createElement('option');d.className='display__time-predefined',d.innerHTML=a[b],c.appendChild(d)}b.appendChild(c),displayArea.appendChild(b)}function displayNextTimers(){if(nextTimers.hasChildNodes()&&(nextTimers.innerHTML=''),1==timers.length){const a=document.createElement('li');a.className='nextTimers__item',a.innerHTML='No more timers',nextTimers.appendChild(a)}else{const a=document.createDocumentFragment();for(let b=timers.length-2;0<=b;b--){const c=document.createElement('li'),d=document.createElement('button');d.className='startBtn',d.setAttribute('data-arrPos',b+1-timers.length),d.innerHTML=timers[b];const e=document.createElement('button');e.className='deleteBtn',e.setAttribute('data-arrPos',b-timers.length),e.innerHTML='delete',c.className='nextTimers__item',c.appendChild(d),c.appendChild(e),a.appendChild(c)}nextTimers.appendChild(a)}}function valuesToArray(a){let b;a.value?(b=a.value,a.value=''):b=a,timers=b.split('+'),timers.reverse()}function timer(a){clearInterval(countdown),displayNextTimers(),displayTotalTime(timers);const b=Date.now();displayTimeLeft(a),countdown=setInterval(()=>(secondsLeft=Math.round((b+1e3*a-Date.now())/1e3),0>secondsLeft?(countdownOver(),timers.pop(),clearInterval(countdown),void(0<timers.length?timer(60*timers[timers.length-1]):timerRunButton.textContent='\u25B6')):void displayTimeLeft(secondsLeft)),1e3)}function timerStart(a){timer(a),timerRunButton.textContent='\u23F8'}function timersRun(){timerInput.value?(valuesToArray(timerInput),timerStart(60*timers[timers.length-1])):timerPauseResume()}function timerPauseResume(){countdown&&('\u23F8'===timerRunButton.textContent?(timerRunButton.textContent='\u25B6',clearInterval(countdown)):timerStart(secondsLeft))}if(3<timersPredefined.length){displayTimersPredefinedSelect(timersPredefined);const a=document.getElementById('predefinedSelect');a.addEventListener('click',(a)=>{valuesToArray(a.target.selectedOptions[0].innerHTML),timerStart(60*timers[timers.length-1])})}else displayTimersPredefined(timersPredefined),displayArea.addEventListener('click',(a)=>{'BUTTON'===a.target.tagName&&(valuesToArray(a.target.innerHTML),timerStart(60*timers[timers.length-1]))});timerRunButton.addEventListener('click',timersRun),timerInput.addEventListener('keypress',function(a){'Enter'===a.key&&(a.preventDefault(),timersRun())}),nextTimers.addEventListener('click',(a)=>{const b=a.target.dataset.arrpos;if('deleteBtn'===a.target.className)timers.splice(b,1),displayNextTimers(),displayTotalTime(timers);else if('startBtn'===a.target.className){timers.splice(b),timers.reverse();let a=timers.join('+');valuesToArray(a),timerStart(60*timers[timers.length-1])}});
+/*jshint esversion: 6 */
+let countdown;
+let secondsLeft;
+let timers = [];
+const timersPredefined = 
+['15+5+15+9+15', 
+'15+15+15',
+'15+10+15', 
+'15+10+15+8+15+10+15'];
+const nextTimers = document.getElementById('nextTimers');
+const displayArea = document.getElementById('displayArea');
+const timerInput = document.getElementById('timerInput');
+const timerRunButton = document.getElementById('timerRun');
+const timerDisplay = document.querySelector('.displayTimeLeft');
+const timerTotalTime = document.querySelector('.displayTimeTotal');
+const soundComplete = document.getElementById('complete');
+
+function countdownOver() {
+    soundComplete.play();
+}
+function secondsToTimeLeftString(seconds) {
+    const minutes = Math.floor(seconds / 60);
+    const remainderSeconds = seconds % 60;
+    const display = `${minutes}:${remainderSeconds < 10 ? '0' : '' }${remainderSeconds}`;
+    return display;
+}
+function displayTimeLeft(seconds) {
+    let timeLeft = secondsToTimeLeftString(seconds);
+    document.title = timeLeft;
+    timerDisplay.textContent = timeLeft;
+}
+function displayTotalTime(arr) {
+    if (arr.length == 1) {
+        timerTotalTime.textContent = '';
+        return;
+    }
+    let total = arr.reduce((a, v) => {
+        return parseInt(a) + parseInt(v);
+    });
+    timerTotalTime.textContent = `total timers time is: ${total} ${total == 1 ? 'minute' : "minutes"}`;
+}
+function displayTimersPredefined(arr) {
+    const insertArea = document.createDocumentFragment();
+    for (let i = 0; i < arr.length; i++) {
+        const insertElement = document.createElement('button');
+        insertElement.className = "display__time-predefined";
+        insertElement.innerHTML = arr[i];
+        insertArea.appendChild(insertElement);
+    }
+    displayArea.appendChild(insertArea);
+}
+function displayTimersPredefinedSelect(arr) {
+    const insertArea = document.createDocumentFragment();
+    const insertSelect = document.createElement('select');
+    insertSelect.setAttribute('id', 'predefinedSelect');
+    for (let i = 0; i < arr.length; i++) {
+        const insertElement = document.createElement('option');
+        insertElement.className = "display__time-predefined";
+        insertElement.innerHTML = arr[i];
+        insertSelect.appendChild(insertElement);
+    }
+    insertArea.appendChild(insertSelect);
+    displayArea.appendChild(insertArea);
+}
+function displayNextTimers() {
+    if (nextTimers.hasChildNodes()) {
+        nextTimers.innerHTML = '';
+    }
+    if (timers.length == 1) {
+        const insertCurrentElement = document.createElement("li");
+        insertCurrentElement.className = "nextTimers__item";
+        insertCurrentElement.innerHTML = 'No more timers';
+        nextTimers.appendChild(insertCurrentElement);
+    } else {
+        const insertArea = document.createDocumentFragment();
+        for (let i = timers.length - 2; i >= 0; i--) {
+            const insertCurrentElement = document.createElement("li");
+            const startBtn = document.createElement('button');
+            startBtn.className = "startBtn";
+            startBtn.setAttribute("data-arrPos", (i + 1) - timers.length);
+            startBtn.innerHTML = timers[i];
+            const deleteBtn = document.createElement('button');
+            deleteBtn.className = "deleteBtn";
+            deleteBtn.setAttribute("data-arrPos", i - timers.length);
+            deleteBtn.innerHTML = "delete";
+            insertCurrentElement.className = "nextTimers__item";
+            insertCurrentElement.appendChild(startBtn);
+            insertCurrentElement.appendChild(deleteBtn);
+            insertArea.appendChild(insertCurrentElement);
+        }
+        nextTimers.appendChild(insertArea);
+    }
+}
+function valuesToArray(timerSet) {
+    let minutes;
+    if (timerSet.value) {
+        minutes = timerSet.value;
+        timerSet.value = '';
+    } else {
+        minutes = timerSet;
+    }
+    timers = minutes.split('+');
+    timers.reverse();
+}
+function timer(seconds) {
+    clearInterval(countdown);
+    displayNextTimers();
+    displayTotalTime(timers);
+    const now = Date.now();
+    const then = now + seconds * 1000;
+    displayTimeLeft(seconds);
+    countdown = setInterval(() => {
+        secondsLeft = Math.round((then - Date.now()) / 1000);
+        if (secondsLeft < 0) {
+            countdownOver();
+            timers.pop();
+            clearInterval(countdown);
+            if (timers.length > 0) {
+                timer(timers[timers.length - 1] * 60);
+            } else {
+                timerRunButton.textContent = '▶';
+            }
+            return;
+        }
+        displayTimeLeft(secondsLeft);
+    }, 1000);
+}
+function timerStart(duration) {
+    timer(duration);
+    timerRunButton.textContent = '⏸';
+}
+function timersRun() {
+    if (timerInput.value) {
+        valuesToArray(timerInput);
+        timerStart(timers[timers.length - 1] * 60);
+    } else {
+        timerPauseResume();
+    }
+}
+function timerPauseResume() {
+    if (!countdown) return;
+    if (timerRunButton.textContent === '⏸') {
+        timerRunButton.textContent = '▶';
+        clearInterval(countdown);
+    } else {
+        timerStart(secondsLeft);
+    }
+}
+
+if (timersPredefined.length > 3) {
+    displayTimersPredefinedSelect(timersPredefined);
+    const predefinedSelect = document.getElementById('predefinedSelect');
+    predefinedSelect.addEventListener('click', e => {
+        valuesToArray(e.target.selectedOptions[0].innerHTML);
+        timerStart(timers[timers.length - 1] * 60);
+    });
+} else{
+    displayTimersPredefined(timersPredefined);
+    displayArea.addEventListener('click', e => {
+        if (e.target.tagName === 'BUTTON') {
+            valuesToArray(e.target.innerHTML);
+            timerStart(timers[timers.length - 1] * 60);
+        }
+    });
+}
+
+timerRunButton.addEventListener('click', timersRun);
+
+timerInput.addEventListener("keypress", function(event) {
+    if (event.key === "Enter") {
+        event.preventDefault();
+        timersRun();
+    }
+});
+nextTimers.addEventListener('click', e => {
+    const pos = e.target.dataset.arrpos;
+    if (e.target.className === 'deleteBtn') {
+        timers.splice(pos, 1);
+        displayNextTimers();
+        displayTotalTime(timers);
+    } else if (e.target.className === 'startBtn') {
+        timers.splice(pos);
+        timers.reverse();
+        let newTimers = timers.join('+');
+        valuesToArray(newTimers);
+        timerStart(timers[timers.length - 1] * 60);
+    }
+});
