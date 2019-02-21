850cb9ab6f5e96997c78bd22b50777858e260853
alnat2 <alnat2@gmail.com>
2019-02-21 14:02:36 +0300

    delete es5 tasks from gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,10 @@
 /*jshint esversion: 6 */
 const gulp = require('gulp');
-//const babel = require('gulp-babel');
-//const rename = require("gulp-rename");
 const minify = require("gulp-babel-minify");
 const htmlmin = require('gulp-html-minifier');
 const csso = require('gulp-csso');
 const plumber = require('gulp-plumber');
 const notify = require("gulp-notify");
-//const liveServer = require('live-server');
 
 const paths = {
 	watch: {
@@ -20,16 +17,6 @@ const serverParams = {
 	port: 4000,
 	watch: 'dev'
 };
-// function toes5() {
-// 	return gulp
-// 		.src('dev/js/timer.js')
-// 		.pipe(plumber({ errorHandler: notify.onError("Error: <%= error.message %>") }))
-//         .pipe(rename('dev/js/timeres5.js'))
-//         .pipe(babel({
-// 			presets: ['@babel/env']
-// 		}))
-// 		.pipe(gulp.dest('.'))
-// }
 
 function minjs() {
 	return gulp
@@ -61,13 +48,6 @@ function watchFiles() {
 	gulp.watch(paths.watch.css, mincss);
 	gulp.watch(paths.watch.html, minhtml);
 	gulp.watch(paths.watch.js, minjs);
-	// gulp.watch(paths.watch.js, gulp.series(toes5, minjs));
   }
-// 
-
-//function liveServerStart() {
-	//liveServer.start(serverParams);
-	//done();
-  //}
+  
 gulp.task('default', gulp.parallel(watchFiles));
-// gulp.task('default', ['reload', 'watch']);
